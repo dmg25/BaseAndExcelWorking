604dc3eae1f88ef502e8e3bce117a29167b55769
dmg25 <43114826+dmg25@users.noreply.github.com>
2018-09-09 19:00:04 +0200

    Add files via upload

---
+++ ConnectToSCADABD/ConnectToSCADABD/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace ConnectToSCADABD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.Table1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Table2 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Table1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Table2)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 86);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Connect&SQL";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Table1
+            // 
+            this.Table1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Table1.Location = new System.Drawing.Point(12, 117);
+            this.Table1.Name = "Table1";
+            this.Table1.Size = new System.Drawing.Size(438, 403);
+            this.Table1.TabIndex = 1;
+            this.Table1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(395, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // Table2
+            // 
+            this.Table2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Table2.Location = new System.Drawing.Point(482, 117);
+            this.Table2.Name = "Table2";
+            this.Table2.Size = new System.Drawing.Size(438, 403);
+            this.Table2.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(859, 22);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(99, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Connect&SQL";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 60);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(194, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "select * from ISAOBJ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Введите SQL запрос";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 19);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(77, 17);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Таблица 1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 42);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(77, 17);
+            this.radioButton2.TabIndex = 6;
+            this.radioButton2.Text = "Таблица 2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Location = new System.Drawing.Point(212, 44);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(177, 67);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "В какой таблице отобразить?";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 21);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(194, 20);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.Text = "C:\\\\SCADABD.GDB";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 5);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Введите адрес базы";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1009, 646);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Table2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Table1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Table1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Table2)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView Table1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView Table2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ ConnectToSCADABD/ConnectToSCADABD/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FirebirdSql.Data.FirebirdClient;
+
+namespace ConnectToSCADABD
+{
+
+    
+    public partial class Form1 : Form
+    {
+
+       
+        DataGridView TmpDG; // переменная для сменяемости таблиц
+        string SQL; // "select * from OBJTYPE "; //Where Speed=@Speed
+        string BaseAddr; //адрес базы
+
+        string ConStr;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            BaseAddr = textBox2.Text;
+
+            ConStr = "character set=WIN1251;initial catalog=" + BaseAddr + ";user id=SYSDBA;password=masterkey"; // наша строка подключения, сделать её изменяемой!!!
+           
+          // SQL = "select * from ISAOBJ";
+           SQL = textBox1.Text;
+
+           if  (radioButton1.Checked) 
+           {
+               TmpDG = Table1;
+           }
+           else if (radioButton2.Checked)
+           {
+               TmpDG = Table2;
+           }
+
+           ConnectToBase();
+        
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ConStr = "character set=WIN1251;initial catalog=C:\\SCADABD.GDB;user id=SYSDBA;password=masterkey"; // наша строка подключения, сделать её изменяемой!!!
+            TmpDG = Table2;
+        //    SQL = "select * from ISAUPCHANNEL";
+            ConnectToBase();
+        }
+
+
+        
+        private void ConnectToBase(/*object sender, EventArgs e*/)
+        {
+            // Строка подключения
+                using (FbConnection fbc = new FbConnection(ConStr))   // используем Using для последующего высвобождения ресурсов, должно быть оптимальнее
+                {
+                    try
+                    {
+                        fbc.Open();                        
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message,
+                            System.IO.Path.GetFileName(
+                            System.Reflection.Assembly.GetExecutingAssembly().Location));
+                        return;
+                    }
+
+                    // Транзакция, которая тупо откуда-то скопирована
+                    FbTransactionOptions fbto = new FbTransactionOptions();
+                    fbto.TransactionBehavior = FbTransactionBehavior.NoWait |
+                         FbTransactionBehavior.ReadCommitted |
+                         FbTransactionBehavior.RecVersion;
+                    FbTransaction fbt = fbc.BeginTransaction(fbto);
+
+                    // Создаем простой запрос
+                   
+                    FbCommand fbcom = new FbCommand(SQL, fbc, fbt);
+                    //            fbcom.Parameters.Clear();
+                    //              fbcom.Parameters.AddWithValue("speed", 100);
+ 
+                    // Создаем адаптер данных
+                    FbDataAdapter fbda = new FbDataAdapter(fbcom);
+
+                    DataSet ds = new DataSet();   //из формы не тащится, видимо там надо задавать способ подключения через стандартные способы, а они не пашут.
+
+                    try
+                    {
+                        fbda.Fill(ds);  // заполняем DataSet
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message,
+                            System.IO.Path.GetFileName(
+                            System.Reflection.Assembly.GetExecutingAssembly().Location));                       
+                        return;
+                    }
+                    finally
+                    {
+                        fbt.Rollback();
+                        fbc.Close();
+                    }
+
+                    DataTable dt = ds.Tables[0];
+
+                    if (dt.Rows.Count == 0) return;
+
+                    
+
+                    TmpDG.DataSource = dt;
+                    
+                   // MessageBox.Show("ololo");
+                    label1.Text = "Имя таблицы: " + dt.TableName + " ; Кол-во столбцов:" + dt.Columns.Count + " ; Кол-во строк:" + dt.Rows.Count;
+                  //  Console.WriteLine("Имя таблицы: " + dt.TableName + " ; Кол-во столбцов:" + dt.Columns.Count + " ; Кол-во строк:" + dt.Rows.Count);
+
+                 /*   for (int i = 0; i < dt.Columns.Count; i++)
+                    {
+                        Console.Write("\t{0}", dt.Columns[i].ColumnName);
+                    }
+
+                    for (int i = 0; i < dt.Rows.Count; i++)
+                    {
+                        DataRow dr = dt.Rows[i];
+
+                        if (dr.IsNull("NAME") == false)
+                        {
+                            // необходимые действия
+                            //  Int32 id = Convert.ToInt32(dr["ID"]);
+
+                            var cells = dt.Rows[i].ItemArray;
+                            foreach (object cell in cells)
+                                Console.Write("\t{0}", cell);
+                            Console.WriteLine();
+
+
+
+                        }
+                    }*/
+                    
+                }
+            
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+       
+
+    }
+}
